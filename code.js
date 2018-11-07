eccfb5fd323a05d5006291350aabaf9edf97a028
pinscolors <pinscolors@gmail.com>
2018-11-07 09:08:18 +0100

    version 2

--- code.js
+++ code.js
@@ -2,3 +2,6 @@ window.addEventListener("load",function () {
   var node = document.querySelector("[data-loading]");
   node.setAttribute("data-loading","complete");
 })
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
+});
