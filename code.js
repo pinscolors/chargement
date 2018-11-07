7237c1ca5b575c77f97d703765d54becbdf0ceb4
pinscolors <pinscolors@gmail.com>
2018-11-07 09:18:11 +0100

    retour a la  1er version
    :/

--- code.js
+++ code.js
@@ -2,6 +2,3 @@ window.addEventListener("load",function () {
   var node = document.querySelector("[data-loading]");
   node.setAttribute("data-loading","complete");
 })
-$(".menu-collapsed").click(function() {
-  $(this).toggleClass("menu-expanded");
-});
